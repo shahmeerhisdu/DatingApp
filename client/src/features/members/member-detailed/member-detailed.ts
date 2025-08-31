5b5a1fd35a78f5d3c0a89b1de18635a6e8e9bdc2
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2025-08-31 19:23:59 +0500

    Edit button added using the member service signal to edit user information

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { filter, Observable } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-member-detailed',
@@ -14,12 +15,19 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
   styleUrl: './member-detailed.css'
 })
 export class MemberDetailed implements OnInit {
-  // private memberService = inject(MemberService);
+  protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
+  private accountService = inject(AccountService);
   private router = inject(Router); // we need to get the hold of the router to get the tile functionality done.
   // protected member$?: Observable<Member>;
   protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
+
+  //edit working
+  // We need a property inside here to check if this is the current user, we will take a look on to another type of singal to do this, and that is the computed signal and a computed signal use another signal to work out what its value should be.
+  protected isCurrentUser = computed(()=>{
+    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id');
+  })
 
   ngOnInit(): void {
     // this.member$ = this.loadMember();
--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
+import { MemberService } from '../../../core/services/member-service';
 
 @Component({
   selector: 'app-member-profile',
@@ -10,7 +11,7 @@ import { DatePipe } from '@angular/common';
   styleUrl: './member-profile.css'
 })
 export class MemberProfile implements OnInit{
-
+  protected memberService = inject(MemberService)
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
 
