d9d6f07c1a781115e77b8d2b5bc5b90a40b1a9c5
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2025-09-13 20:19:08 +0500

    added the caching and end of section 10

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -1,15 +1,26 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { BusyService } from '../services/busy-service';
-import { delay, finalize } from 'rxjs';
+import { delay, finalize, of, tap } from 'rxjs';
+
+const cache = new Map<string, HttpEvent<unknown>>(); // we gonna cache based on the string and the string in this case is going to represent the URL the request is going to and we are going to cache the get request we are not interested in caching that is not a get request
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService)
 
+  if(req.method === 'GET'){
+    const cachedResponse = cache.get(req.url);
+    if(cachedResponse){
+      return of(cachedResponse);
+    }
+  }
   busyService.busy();
 
   return next(req).pipe(
     delay(500), 
+    tap(response => {
+      cache.set(req.url, response)
+    }),
     finalize(() => {
       busyService.idel();
     })
