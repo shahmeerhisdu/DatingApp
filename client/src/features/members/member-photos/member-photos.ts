a3cbb43aec8d368c2895ce93e2ddfde764284efc
Muhammad Shahmeer <140377186+shahmeerhisdu@users.noreply.github.com>
2025-12-12 01:08:45 +0500

    end of section 11, done with the photo delete as well on UI side

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -8,16 +8,17 @@ import { ImageUpload } from '../../../shared/image-upload/image-upload';
 import { AccountService } from '../../../core/services/account-service';
 import { User } from '../../../types/user';
 import { StartButton } from "../../../shared/start-button/start-button";
+import { DeleteButton } from "../../../shared/delete-button/delete-button";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload, StartButton],
+  imports: [ImageUpload, StartButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
 
   // protected photos$?: Observable<Photo[]>;
@@ -75,6 +76,14 @@ export class MemberPhotos implements OnInit {
           ...member,
           imageUrl: photo.url
         }) as Member)
+      }
+    })
+  }
+
+  deletePhoto(photoId: number){
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update(photos => photos.filter(x => x.id != photoId))
       }
     })
   }
