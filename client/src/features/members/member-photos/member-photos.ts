31ad98c593902806f4506bf6215d34f2f490fcfa
Muhammad Shahmeer <140377186+shahmeerhisdu@users.noreply.github.com>
2025-12-11 21:05:41 +0500

    done with the fav image as the main image

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -2,18 +2,21 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Photo } from '../../../types/member';
+import { Member, Photo } from '../../../types/member';
 import { AsyncPipe } from '@angular/common';
 import { ImageUpload } from '../../../shared/image-upload/image-upload';
+import { AccountService } from '../../../core/services/account-service';
+import { User } from '../../../types/user';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [AsyncPipe, ImageUpload],
+  imports: [ImageUpload],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
+  private accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
 
   // protected photos$?: Observable<Photo[]>;
@@ -55,6 +58,22 @@ export class MemberPhotos implements OnInit {
       error: error => {
         console.log('Error uploading image', error);
         this.loading.set(false);
+      }
+    })
+  }
+
+  setMainPhoto(photo: Photo){
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: ()=> {
+        const currentUser = this.accountService.currentUser();
+        if(currentUser){
+          currentUser.imageUrl = photo.url;
+        }
+        this.accountService.setCurrentUser(currentUser as User)
+        this.memberService.member.update(member => ({
+          ...member,
+          imageUrl: photo.url
+        }) as Member)
       }
     })
   }
