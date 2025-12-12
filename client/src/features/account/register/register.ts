628a96305d78e58ef9513848bdbc4a6e7aaa2278
Muhammad Shahmeer <140377186+shahmeerhisdu@users.noreply.github.com>
2025-12-13 01:01:29 +0500

    Changing to reactive forms

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,28 +1,46 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterCreds, User } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit {
+  
   private accountService = inject(AccountService)
   // membersFromHome = input.required<User[]>(); // this is the other way of getting the data from the parent.
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds
+  protected registerForm: FormGroup =  new FormGroup({});
 
-  register(){
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
+  ngOnInit(): void {
+    this.initializeRegisterForm();
+  }
+
+  initializeRegisterForm(){
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      displayName: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
     })
+  }
+  
+  register(){
+
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => console.log(error)
+    // })
   }
 
   cancel(){
