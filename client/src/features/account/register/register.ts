1ab7da118cc4bb9b78d1a4d35db432c922254b01
Muhammad Shahmeer <140377186+shahmeerhisdu@users.noreply.github.com>
2025-12-13 02:40:35 +0500

    Done with the custom validation

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,5 +1,5 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds, User } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
@@ -11,27 +11,45 @@ import { JsonPipe } from '@angular/common';
   styleUrl: './register.css'
 })
 export class Register implements OnInit {
-  
+
   private accountService = inject(AccountService)
   // membersFromHome = input.required<User[]>(); // this is the other way of getting the data from the parent.
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds
-  protected registerForm: FormGroup =  new FormGroup({});
+  protected registerForm: FormGroup = new FormGroup({});
 
   ngOnInit(): void {
     this.initializeRegisterForm();
   }
 
-  initializeRegisterForm(){
+  initializeRegisterForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl(),
-      displayName: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
+      email: new FormControl("Shahmeer@gmail.com", [Validators.required, Validators.email]),
+      displayName: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+
+    //custom validation only occurs on the confirm password if we correct the password after confirm password we will get invalid because the confirm password does not know that the password field has been changed so we need some mechanism for that as well that is done in here.
+
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
     })
   }
-  
-  register(){
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+
+      if (!parent) return null;
+
+      const matchValue = parent.get(matchTo)?.value;
+
+      return control.value === matchValue ? null : { passwordMismatch: true }
+    }
+  }
+
+  register() {
 
     console.log(this.registerForm.value);
     // this.accountService.register(this.creds).subscribe({
@@ -43,7 +61,7 @@ export class Register implements OnInit {
     // })
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
   }
 }
--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -10,7 +10,7 @@ import { User } from '../../types/user';
 })
 export class Home {
   // @Input({required: true})  membersFromApp : User[] = []
-  protected registerMode = signal(false);
+  protected registerMode = signal(true);
 
   showRegister(value: boolean){
     this.registerMode.set(value);
