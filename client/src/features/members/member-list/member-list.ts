eadf79aaa34d7871e52701c85ab51fb2b5f7bc1b
Muhammad Shahmeer <140377186+shahmeerhisdu@users.noreply.github.com>
2025-12-16 23:39:14 +0500

    Pagination on client side
    Update in API.

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AsyncPipe } from '@angular/common';
 import { MembersCard } from "../members-card/members-card";
+import { PaginatedResult } from '../../../types/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -13,9 +14,9 @@ import { MembersCard } from "../members-card/members-card";
 })
 export class MemberList {
   private memberService = inject(MemberService);
-  protected members$ : Observable<Member[]>; // by convention we need to add the $ sign for the observeable, and if we don't initialize this in the constructor we will get the error, but because we are not going to be making any fetch request from this memberlist component that is happening inside the service, and typically we do fetch operation inside an angular cycle oninit method, but instead of faking this intialization with an empty array we can use that in ihe constructor.
+  protected paginatedMembers$ : Observable<PaginatedResult<Member>>; // by convention we need to add the $ sign for the observeable, and if we don't initialize this in the constructor we will get the error, but because we are not going to be making any fetch request from this memberlist component that is happening inside the service, and typically we do fetch operation inside an angular cycle oninit method, but instead of faking this intialization with an empty array we can use that in ihe constructor.
 
   constructor(){
-    this.members$ = this.memberService.getMembers();
+    this.paginatedMembers$ = this.memberService.getMembers();
   }
 }
