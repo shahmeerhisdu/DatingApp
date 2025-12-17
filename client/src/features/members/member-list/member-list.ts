760baacec43b61fdc47306862838f8705ca174f1
Muhammad Shahmeer <140377186+shahmeerhisdu@users.noreply.github.com>
2025-12-18 01:13:17 +0500

    refactoring of member list component

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
@@ -13,19 +13,22 @@ import { Paginator } from "../../../shared/paginator/paginator";
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
-export class MemberList {
+export class MemberList implements OnInit {
   private memberService = inject(MemberService);
-  protected paginatedMembers$? : Observable<PaginatedResult<Member>>; // by convention we need to add the $ sign for the observeable, and if we don't initialize this in the constructor we will get the error, but because we are not going to be making any fetch request from this memberlist component that is happening inside the service, and typically we do fetch operation inside an angular cycle oninit method, but instead of faking this intialization with an empty array we can use that in ihe constructor.
+  protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
 
   pageNumber = 1;
   pageSize = 5;
 
-  constructor(){
+  
+  ngOnInit(): void {
     this.loadMembers();
   }
 
   loadMembers(){
-    this.paginatedMembers$ = this.memberService.getMembers(this.pageNumber, this.pageSize);
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: (result) => this.paginatedMembers.set(result)
+    });
   }
 
   onPageChange(event: { pageNumber: number, pageSize: number }){
