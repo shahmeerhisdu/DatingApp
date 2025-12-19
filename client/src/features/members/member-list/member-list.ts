1a4a8705e01da25cd5a1574ef780d104bded68c9
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2025-12-20 02:56:25 +0500

    modal filter part 1 working with dialog

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,24 +1,25 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { Observable } from 'rxjs';
-import { Member } from '../../../types/member';
+import { Member, MemberParams } from '../../../types/member';
 import { AsyncPipe } from '@angular/common';
 import { MembersCard } from "../members-card/members-card";
 import { PaginatedResult } from '../../../types/pagination';
 import { Paginator } from "../../../shared/paginator/paginator";
+import { FilterModal } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe, MembersCard, Paginator], //handle the subscription of an observeable.
+  imports: [MembersCard, Paginator, FilterModal], //handle the subscription of an observeable.
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
 export class MemberList implements OnInit {
+  @ViewChild('filterModal') modal!: FilterModal;
   private memberService = inject(MemberService);
   protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
 
-  pageNumber = 1;
-  pageSize = 5;
+  protected memberParams = new MemberParams();
 
   
   ngOnInit(): void {
@@ -26,14 +27,31 @@ export class MemberList implements OnInit {
   }
 
   loadMembers(){
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    this.memberService.getMembers(this.memberParams).subscribe({
       next: (result) => this.paginatedMembers.set(result)
     });
   }
 
   onPageChange(event: { pageNumber: number, pageSize: number }){
-    this.pageNumber = event.pageNumber;
-    this.pageSize = event.pageSize;
+    this.memberParams.pageNumber = event.pageNumber;
+    this.memberParams.pageSize = event.pageSize;
+    this.loadMembers();
+  }
+
+  openModal(){
+    this.modal.open();
+  }
+
+  onClose(){
+    console.log('Modal closed');
+  }
+
+  onFilterChange(data: MemberParams){
+    console.log('Modal submitted data: ', data);
+  }
+
+  resetFilters(){
+    this.memberParams = new MemberParams();
     this.loadMembers();
   }
 }
