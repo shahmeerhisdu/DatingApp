98e42c57ea43a7a9486c7b05d3229f931bc3f4ba
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2025-12-22 01:20:09 +0500

    UTC date time fixed in the dbContext

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 using System;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data;
 
@@ -16,4 +17,27 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
+
+    //the databases except the postgresql don't support the UTC datetime natively so we need to configure it manually, and will need to override the method that is inside the DbContext class
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //we can use this when we need to configure or override the entity framework conventions
+        base.OnModelCreating(modelBuilder);
+
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc) //when we are reading from the database we need to specify that this is in UTC format
+        );
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+            }
+        }
+    }
 }
