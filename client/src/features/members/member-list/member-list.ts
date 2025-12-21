961528f8dac50681ddc74fe6892547abfbf745dd
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2025-12-21 21:21:28 +0500

    done with displaying the selected filters on the members list component

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -20,6 +20,7 @@ export class MemberList implements OnInit {
   protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
 
   protected memberParams = new MemberParams();
+  private updatedParams = new MemberParams();
 
   
   ngOnInit(): void {
@@ -47,12 +48,32 @@ export class MemberList implements OnInit {
   }
 
   onFilterChange(data: MemberParams){
-    this.memberParams = data;
+    this.memberParams = {...data}; //If I do = data on both it will reference the same object and changes in one will affect the other. So by using the speread operator we create a shallow copy.
+    this.updatedParams = {...data};
     this.loadMembers();
   }
 
   resetFilters(){
     this.memberParams = new MemberParams();
     this.loadMembers();
+  }
+
+  get displayMessage(): string{
+    const defaultParams = new MemberParams();
+
+    const filters: string[] = [];
+
+    if(this.updatedParams.gender){
+      filters.push(this.updatedParams.gender + 's');
+    }else{
+      filters.push('Males, Females');
+    }
+
+    if(this.updatedParams.minAge !== defaultParams.minAge || this.updatedParams.maxAge !== defaultParams.maxAge){
+      filters.push(` ages ${this.updatedParams.minAge} - ${this.updatedParams.maxAge}`);
+    }
+
+    filters.push(this.updatedParams.orderBy === 'lastActive' ? ' Recently Active' : ' Newest Members');
+    return filters.length > 0 ?  `Selected Filters: ${filters.join('  | ')}` : 'All Members';
   }
 }
