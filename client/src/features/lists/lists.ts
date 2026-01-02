ba5695dcec67211be2de5498eb681fb42a4414cc
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2026-01-03 01:48:19 +0500

    showing member list for liked, liked by, mutual likes

--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { LikesService } from '../../core/services/likes-service';
+import { Member } from '../../types/member';
+import { MembersCard } from "../members/members-card/members-card";
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MembersCard],
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
-export class Lists {
+export class Lists implements OnInit {
 
+  private likeService = inject(LikesService);
+  protected members = signal<Member[]>([]);
+  protected predicate = 'liked';
+
+  tabs = [
+    { label: 'Liked', value: 'liked' },
+    { label: 'Liked Me', value: 'likedBy' },
+    { label: 'Mutual', value: 'mutual' }
+  ];
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  setPredicate(predicate: string){
+    if(this.predicate !== predicate){
+      this.predicate = predicate;
+      this.loadLikes();
+    }
+  }
+
+  loadLikes(){
+    this.likeService.getLikes(this.predicate).subscribe({
+      next: members => {
+        this.members.set(members);
+      }
+    });    
+  }
 }
