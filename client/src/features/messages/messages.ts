3fcd0ceb1514d210e567f5de6e73569532b77641
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2026-01-18 21:03:55 +0500

    Displaying messages in the UI

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MessageService } from '../../core/services/message-service';
+import { PaginatedResult } from '../../types/pagination';
+import { Message } from '../../types/message';
 
 @Component({
   selector: 'app-messages',
@@ -6,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
-export class Messages {
+export class Messages implements OnInit {
+  private messageService = inject(MessageService);
+  protected container = 'Inbox';
+  protected pageNumber = 1;
+  protected pageSize = 10;
 
+  protected paginatedMessages = signal<PaginatedResult<Message> | null>(null); 
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  protected loadMessages(): void {
+    this.messageService.getMessages(this.container, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: (paginatedResult) => {
+          this.paginatedMessages.set(paginatedResult);
+        },
+        error: (error) => {
+          console.error('Error loading messages:', error);
+        }
+      });
+  }
 }
