cf60fa33c3f181b9309b583a5f1ab8e12b23ae36
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2026-01-22 23:06:52 +0500

    Message Component setup to receive the messages

--- client/src/features/members/member-messages/member-messages.ts
+++ client/src/features/members/member-messages/member-messages.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MessageService } from '../../../core/services/message-service';
+import { MemberService } from '../../../core/services/member-service';
+import { Message } from '../../../types/message';
+import { DatePipe } from '@angular/common';
+import { TimeAgoPipe } from '../../../core/pipes/time-ago-pipe';
 
 @Component({
   selector: 'app-member-messages',
-  imports: [],
+  imports: [DatePipe, TimeAgoPipe],
   templateUrl: './member-messages.html',
   styleUrl: './member-messages.css'
 })
-export class MemberMessages {
+export class MemberMessages implements OnInit {
+  private messageService = inject(MessageService);
+  private memberService = inject(MemberService);
 
+  protected messages = signal<Message[]>([]);
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  private loadMessages(): void {
+    const memberId = this.memberService.member()?.id;
+    if (memberId) {
+      this.messageService.getMessageThread(memberId).subscribe({
+        next: (messages) => {
+          this.messages.set(messages.map(message => ({
+            ...message,
+            currentUserSender: message.senderId !== memberId
+          })));
+        }
+      });
+    }
+  }
 }
