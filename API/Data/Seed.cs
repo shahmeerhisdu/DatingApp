6f4c85773d51d05636e26fe8b00ef4d229b7ab26
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2026-01-24 23:00:02 +0500

    Done with dropping old db and seeding new data.

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -4,15 +4,16 @@ using System.Text;
 using System.Text.Json;
 using API.DTOs;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(AppDbContext context)
+    public static async Task SeedUsers(UserManager<AppUser> userManager)
     {
-        if (await context.Users.AnyAsync()) return;
+        if (await userManager.Users.AnyAsync()) return;
 
         var memberData = await File.ReadAllTextAsync("Data/UserSeedData.json"); // this is just the string so deceralize it
 
@@ -26,8 +27,6 @@ public class Seed
 
         foreach (var member in members)
         {
-            using var hmac = new HMACSHA512();
-
             var user = new AppUser
             {
                 Id = member.Id,
@@ -57,9 +56,23 @@ public class Seed
 
             });
 
-            context.Users.Add(user);
+            var result = await userManager.CreateAsync(user, "Pa$$w0rd");
+            if (!result.Succeeded)
+            {
+                Console.WriteLine(result.Errors.First().Description);
+            }
+
+            await userManager.AddToRoleAsync(user, "Member");
         }
 
-        await context.SaveChangesAsync();
+        var admin = new AppUser
+        {
+            UserName = "admin@test.com",
+            Email = "admin@test.com",
+            DisplayName = "Admin"
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
     }
 }
