f7ca935bc553e5b00fdd7cc6f30dd6c9fdd506fb
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2026-02-24 03:01:06 +0500

    Added hub connection to the message service

--- client/src/core/services/message-service.ts
+++ client/src/core/services/message-service.ts
@@ -1,15 +1,51 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Message } from '../../types/message';
 import { PaginatedResult } from '../../types/pagination';
+import { AccountService } from './account-service';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   private baseUrl = environment.apiUrl;
+  //we will need few things to inject, for signalR we need to send the Token with the request to negociate the connection
+  private hubUrl = environment.hubUrl;
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
+  private hubConnection? : HubConnection;
+  // we also want to store here the meesage thread, because we will use the signal inside our message service so that when we do receive that information from the signalR hub we just update the message thread inside here with the signal so that our component can react to that change.
+  messageThread = signal<Message[]>([]);
+
+  createHubConnection(otherUserId: string){
+    const currentUser = this.accountService.currentUser();
+    if(!currentUser) return;
+
+    this.hubConnection = new HubConnectionBuilder().withUrl(this.hubUrl + 'messages?userId=' + otherUserId, {
+      accessTokenFactory: () => currentUser.token
+    })
+    .withAutomaticReconnect()
+    .build();
+
+    this.hubConnection.start().catch(e => console.log("Error: ", e));
+
+    //listen to the events from the signalR hub
+    this.hubConnection.on('ReceivedMessageThread', (messages: Message[]) => {
+      this.messageThread.set(messages.map(message => ({
+            ...message,
+            currentUserSender: message.senderId !== otherUserId
+          })));
+    })
+  }
+
+  stopHubConnection(){
+    if(this.hubConnection?.state === HubConnectionState.Connected){
+      this.hubConnection.stop().catch(e => console.log(e));
+    }
+  }
 
   getMessages(container: string, pageNumber: number, pageSize: number) {
     let params = new HttpParams();
